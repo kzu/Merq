49d2075fdedbfeeb3a58cd8e85ab69288cb8c027
Daniel Cazzulino <daniel@cazzulino.com>
2017-06-15 09:27:55 -0300

    Migrate to XVSSDK and SDK style

--- src/Vsix/Merq.Vsix/Components/AsyncManagerProvider.cs
+++ src/Vsix/Merq.Vsix/Components/AsyncManagerProvider.cs
@@ -66,20 +66,3 @@ namespace Microsoft.VisualStudio.Threading
 	{
 	}
 }
-
-namespace Microsoft.VisualStudio.Shell.Interop
-{
-	/// <summary>
-	/// Internal.
-	/// </summary>
-	[ComImport, Guid("8176DC77-36E2-4987-955B-9F63C6F3F229"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown), TypeIdentifier]
-	public interface IVsTaskSchedulerService2
-	{
-		/// <summary>
-		/// Internal.
-		/// </summary>
-		[return: MarshalAs(UnmanagedType.IUnknown)]
-		[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
-		object GetAsyncTaskContext();
-	}
-}
